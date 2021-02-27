b8a10710549de1c3781b2c3cc3668a57e9f39f7f
Ismail Al Kamal <ismail@nawatt.com>
2021-02-27 08:06:27 +0300

    fixed add user #4

--- api/users/usersModel.js
+++ api/users/usersModel.js
@@ -38,7 +38,7 @@ function getUserByEmail(email) {
 }
 
 async function addUser(newUser) {
-  const [user_id] = await db("users").insert(newUser);
+  const user_id = await db("users").insert(newUser);
   console.log(user_id, [user_id]);
   const [user] = await getUserById(user_id);
   return user;
