4f89b9ec0cbb81f45a457cbacc09381dc16e4328
Ismail Al Kamal <ismail@nawatt.com>
2021-02-27 08:49:08 +0300

    fixed add item #3

--- api/items/itemsModel.js
+++ api/items/itemsModel.js
@@ -44,8 +44,9 @@ function getItemById(id) {
 
 async function addItem(newItem) {
   console.log("NEW ITEM:", newItem);
-  const [newId] = await db("items").insert(newItem);
+  const newId = await db("items").insert(newItem);
   console.log("NEW: ", newId);
+  console.log("NEW: ", [newId]);
   return getItemById(newId.id ?? newId);
 }
 
